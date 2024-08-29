ac91ac93931bca5cb5e526f6cadbbcafb7dd70f2
Marci Prescott <mjprescott@me.com>
2024-08-29 13:56:21 -0600

    Added button and icons

--- script.js
+++ script.js
@@ -1,48 +1,35 @@
-// Get form 
 const itemForm = document.getElementById('item-form');
-// Get input element
-const itemInput = document.getElementById('item-input');
+const itemInput = document.getElementById('item-input')
 const itemList = document.getElementById('item-list');
 
 function addItem(e) {
     e.preventDefault();
-    // Validate input
-
     const newItem = itemInput.value;
 
+    // Validate Input
     if (newItem === '') {
-        alert('Please add an item');
+        alert('Please enter an item');
         return;
     }
-        // Create new list item
-        const li = document.createElement('li');
-        li.appendChild(document.createTextNode(newItem));
+    const li = document.createElement('li');
+    li.appendChild(document.createTextNode(newItem));
 
-        const button = createButton("remove-item btn-link text-red");
-        li.appendChild(button);
-
-        itemList.appendChild(li);
-
-        itemInput.value = '';
-        
-
-    };
-
-    function createButton(classes) {
-        const button = document.createElement('button');
-        button.className = classes;
-        const icon = createIcon('fa-solid fa-xmark');
-        button.appendChild(icon);
-        return button;
-    }
-    function createIcon(classes) {
-        const icon = document.createElement('i');
-        icon.className = classes;
-        return icon;
-    }
-
-
-
-// Event listeners
-
+    const button = createButton('remove-item btn-link text-red'); 
+    li.appendChild(button);
+    itemList.appendChild(li);
+    itemInput.value = '';
+}
+function createButton(classes) {
+    const button = document.createElement('button');
+    button.className = classes;
+    const icon = createIcon('fa-solid fa-xmark');
+    button.appendChild(icon);
+    return button;
+}
+function createIcon(classes) {
+    const icon = document.createElement('i');
+    icon.className = classes;
+    return icon;
+}
+// Event Listeners
 itemForm.addEventListener('submit', addItem);
